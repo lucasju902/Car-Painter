3fcf7cbde862e5a851e7110e5243acf6184f6a97
lucasju902 <lucasju902@gmail.com>
2021-03-26 15:28:24 -0400

    update config

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,13 @@
 const config = {
   env: process.env.NODE_ENV,
-  // apiURL: "/api",
-  assetsURL: process.env.REACT_APP_ASSET_URL || "http://localhost:3000/assets",
-  apiURL: process.env.REACT_APP_BACKEND_ENDPOINT
-    ? process.env.REACT_APP_BACKEND_ENDPOINT + "/api"
-    : "http://localhost:3000/api",
+  assetsURL:
+    process.env.REACT_APP_ASSET_URL ||
+    (process.env.NODE_ENV !== "development"
+      ? "/assets"
+      : "http://localhost:3000/assets"),
+  apiURL:
+    process.env.NODE_ENV !== "development"
+      ? "/api"
+      : "http://localhost:3000/api",
 };
 export default config;
